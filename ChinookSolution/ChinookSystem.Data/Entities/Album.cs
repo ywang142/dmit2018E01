861e405bc67f1fd195281c6547c2fb370b6dc4e3
ywang142 <46543398+ywang142@users.noreply.github.com>
2019-09-09 20:49:03 -0600

    1517setup

--- ChinookSolution/ChinookSystem.Data/Entities/Album.cs
+++ ChinookSolution/ChinookSystem.Data/Entities/Album.cs
@@ -4,9 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ChinookSystem
+namespace ChinookSystem.Data.Entities
 {
-    public class Class1
+    class Album
     {
     }
 }
--- ChinookSolution/ChinookSystem/DAL/ChinookContext.cs
+++ ChinookSolution/ChinookSystem/DAL/ChinookContext.cs
@@ -4,9 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ChinookSystem.Data
+namespace ChinookSystem.DAL
 {
-    public class Class1
+    class ChinookContext
     {
     }
 }
