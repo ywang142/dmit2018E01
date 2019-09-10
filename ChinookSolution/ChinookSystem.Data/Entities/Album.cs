495bf477205fc0cf19e1dbd679f70906ec227b82
ywang142 <46543398+ywang142@users.noreply.github.com>
2019-09-09 20:49:43 -0600

    1517setup2

--- ChinookSolution/ChinookSystem.Data/Entities/Album.cs
+++ ChinookSolution/ChinookSystem.Data/Entities/Album.cs
@@ -4,9 +4,44 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+#region Additional Namespaces
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+#endregion
+
 namespace ChinookSystem.Data.Entities
 {
-    class Album
+    [Table("Albums")]
+    public class Album
     {
+        [Key]
+        public int AlbumID { get; set; }
+        public string Title { get; set; }
+        public int ArtistID { get; set; }
+        public int ReleaseYear { get; set; }
+
+        private string _ReleaseLabel;
+        public string ReleaseLabel
+        {
+            get
+            {
+                return _ReleaseLabel;
+            }
+            set
+            {
+                if (string.IsNullOrEmpty(value))
+                {
+                    _ReleaseLabel = null;
+                }
+                else
+                {
+                    _ReleaseLabel = value;
+                }
+            }
+        }
+
+        
+        public virtual Artist Artist { get; set; }
+
     }
 }
--- ChinookSolution/ChinookSystem.Data/Entities/Artist.cs
+++ ChinookSolution/ChinookSystem.Data/Entities/Artist.cs
@@ -4,9 +4,51 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+#region Additional Namespaces
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+#endregion
+
+
+
 namespace ChinookSystem.Data.Entities
 {
-    class Artist
+    [Table("Artists")]
+    public class Artist
     {
-    }
-}
+        // compound key [Key, Column(Order = n)] where n equals to the physical order of field in database
+        // [Key, DatabaseGenerated(DatabaseGeneratedOption.Computed] database generate a value when a row is inserted or updated
+        // [Key, DatabaseGenerated(DatabaseGeneratedOption.None] database does not generate values
+        // [Key, DatabaseGenerated(DatabaseGeneratedOption.Identity] database generate a value when a row is inserted
+        // [Key] uniquly identities an entity
+        [Key]
+        public int ArtistID { get; set; }
+
+        // fully implemented property
+        private string _Name;
+        public string Name
+        {
+            get
+            {
+                return _Name;
+            }
+            set
+            {
+                if(string.IsNullOrEmpty(value))
+                {
+                    _Name = null;
+                }
+                else
+                {
+                    _Name = value;
+                }//eof
+            }
+        }//eop
+
+        public virtual ICollection<Album> Albums { get; set; }
+
+
+
+
+    } //eoc
+} // eon
