6df33f40d40f3604f3f467ea6c8cf75463e2b61b
ywang142 <46543398+ywang142@users.noreply.github.com>
2019-09-11 20:45:23 -0600

    CRUD

---
+++ ChinookSolution/WebApp/SamplePages/FilterSearchCRUD.aspx.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+#region Additional Namespaces
+using ChinookSystem.BLL;
+using ChinookSystem.Data.Entities;
+
+#endregion
+
+namespace WebApp.SamplePages
+{
+    public partial class FilterSearchCRUD : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if(!Page.IsPostBack)
+            {
+                BindArtistList();
+            }
+        }
+
+        protected void BindArtistList()
+        {
+            ArtistController sysmgr = new ArtistController();
+            List<Artist> info = sysmgr.Artist_List();
+            // aec order
+            info.Sort((x, y) => x.Name.CompareTo(y.Name));
+            // dec order
+            // info.Sort((x, y) => y.Name.CompareTo(x.Name));
+            ArtistList.DataSource = info;
+            ArtistList.DataTextField = nameof(Artist.Name);
+            ArtistList.DataValueField = nameof(Artist.ArtistID);
+            ArtistList.DataBind();
+            ArtistList.Items.Insert(0, "select ...");
+
+
+        }
+    }
+}
