ef1c8400ccb3bd661c97cc59f16835b9d15f6d2a
ywang142 <46543398+ywang142@users.noreply.github.com>
2019-09-18 19:18:30 -0600

    add Album Add, Update, Delete

--- ChinookSolution/ChinookSystem/BLL/AlbumController.cs
+++ ChinookSolution/ChinookSystem/BLL/AlbumController.cs
@@ -15,8 +15,10 @@ namespace ChinookSystem.BLL
     [DataObject]
     public class AlbumController
     {
+        #region Queries
         public List<Album> Album_List()
         {
+            
             using (var context = new ChinookContext())
             {
                 return context.Albums.ToList();
@@ -42,5 +44,44 @@ namespace ChinookSystem.BLL
                 return results.ToList();
             }
         }
+        #endregion
+
+        #region Add, Update Delete
+        public int Album_Add(Album item)
+        {
+            using(var context = new ChinookContext())
+            {
+                context.Albums.Add(item);   // staging
+                context.SaveChanges();      // committed
+                return item.AlbumID;        // return new id value
+            }
+        }
+        public int Album_Update(Album item)
+        {
+            using (var context = new ChinookContext())
+            {
+                context.Entry(item).State = System.Data.Entity.EntityState.Modified;
+                return context.SaveChanges();
+            }
+        }
+
+        public int Album_Delete(int albumid)
+        {
+            using (var context = new ChinookContext())
+            {
+                var existing = context.Albums.Find(albumid);
+                if (existing == null)
+                {
+                    throw new Exception("Album not on file. Delete unnecessary.");
+                }
+                else
+                {
+                    context.Albums.Remove(existing);
+                    return context.SaveChanges();
+                }
+
+            }
+        }
+        #endregion
     }
 }
