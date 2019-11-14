fa29e797775d4cf268d677e80bce452e59a83af8
ywang142 <46543398+ywang142@users.noreply.github.com>
2019-11-13 20:49:01 -0700

    SecurityComplete

---
+++ ChinookSolution/ChinookSystem/BLL/EmployeeController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+#region Additional Namespaces
+using ChinookSystem.Data.Entities;
+using ChinookSystem.Data.DTOs;
+using ChinookSystem.Data.POCOs;
+using ChinookSystem.DAL;
+using System.ComponentModel;
+using DMIT2018Common.UserControls;
+#endregion
+
+namespace ChinookSystem.BLL
+{
+    public class EmployeeController
+    {
+        public List<string> Employees_GetTitles()
+        {
+            using(var context = new ChinookContext())
+            {
+                var results = (from x in context.Employees
+                               select x.Title).Distinct();
+                return results.ToList();
+            }
+
+        }
+    }
+}
